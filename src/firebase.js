893d07ed475d8467e63fe2506d85922e67a3112e
apedocente-maker <ape.docente@gmail.com>
2026-09-01 15:00:26 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-
+import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: "AIzaSyAXdxecrCgI9Iyl2fd-Qn4h99WClSc19Ro",
   authDomain: "pemc-digital.firebaseapp.com",
@@ -14,3 +14,4 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const db = getFirestore(app);
+export const auth = getAuth(app);
